10188b64bda6ae1152709b16fbc0dc9051943442
radekBednarik <radek.bednarik@tesena.com>
2025-01-31 18:51:03 +0100

    add exit() repl command

--- repl/repl.go
+++ repl/repl.go
@@ -6,9 +6,11 @@ import (
 	lexer "github.com/radekBednarik/waiigo/lexer"
 	token "github.com/radekBednarik/waiigo/token"
 	"io"
+	"os"
 )
 
 const PROMPT = ">> "
+const EXIT = "exit()"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,6 +24,10 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+
+		if line == EXIT {
+			os.Exit(0)
+		}
 		l := lexer.New(line)
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
