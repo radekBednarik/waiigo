3ce7c47aafb153205df31f49e704236193b92aaa
radekBednarik <radek.bednarik@tesena.com>
2025-01-31 14:18:07 +0100

    add repl

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	repl "github.com/radekBednarik/waiigo/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Print("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
